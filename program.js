fc3bf60e94439fc8929c5a5745fe53606044acd9
Josh Archer <jarcher@greenriver.edu>
2024-09-24 12:04:33 -0700

    Add documentation

--- program.js
+++ program.js
@@ -1,2 +1,8 @@
+/**
+ * This page is an example of Node.js app
+ * 
+ * @author Josh Archer
+ * @version https://github.com/joshbarcher/test-project-vs-code
+ */
 console.log("Hello, from Node.js!");
 console.log("Nice to see you!");
